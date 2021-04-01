49c8b98fa84efeb405afbda1bd2a598ea290a87d
DacianSky <sdqvsqiu@gmail.com>
2021-04-01 14:36:46 +0800

    init

---
+++ TestTravis/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  TestTravis
+//
+//  Created by day on 2021/4/1.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ TestTravis/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  TestTravis
+//
+//  Created by day on 2021/4/1.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
